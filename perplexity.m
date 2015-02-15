61d5379a212c85acd293cdac5aad37423b57d637
Barry Chen <Barry.Y.Chen.16@dartmouth.edu>
2015-02-15 14:31:11 -0500

    added small directories pass_small and fail_small for testing

--- perplexity.m
+++ perplexity.m
@@ -1,7 +1,8 @@
-function [ output_args ] = perplexity( z_t )
+function [ output_args ] = perplexity( z_t, K, alpha )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
+output_args = 1;
 
 end
 
