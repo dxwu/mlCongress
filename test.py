375454fcba7600871a8f1105a5bc3645f673e8b9
Barry Chen <Barry.Y.Chen.16@dartmouth.edu>
2015-03-08 14:12:42 -0400

    save downloads

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import os
 import parse_testbill
 import subprocess
 
-for k in range(7,10):
-	subprocess.call(["python extract_words.py ./pass_training/ ./fail_training/ " + str(k)], shell=True);
+for k in [8,11]:
+	#subprocess.call(["python extract_words.py ./pass_training/ ./fail_training/ " + str(k)], shell=True);
 	subprocess.call(["/Applications/MATLAB_R2014a.app/bin/matlab -nosplash -nodisplay -r \"lda("+str(k)+",0.1,0.1,30,15);quit()\""],shell=True)
 	subprocess.call(["python parse_testbill.py ./pass_test/ ./fail_test/ " + str(k)], shell=True)
 	subprocess.call(["/Applications/MATLAB_R2014a.app/bin/matlab -nosplash -nodisplay -r \"classify(" + str(k) + ", 0.1, 0.1, 30, 15, ['./pass_test_clean/'; './fail_test_clean/']);quit()\""], shell=True)
