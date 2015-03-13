cdfd7164361f104d38576ab79069b891d00d1798
Barry Chen <Barry.Y.Chen.16@dartmouth.edu>
2015-03-12 22:21:48 -0400

    cv_files

---
+++ cv_files/k10/graph_cv.m
@@ -0,0 +1,198 @@
+function graph_cv()
+
+min_error = 1;
+min_params = [0,0,0]; %k,d,s
+
+colors = ['-*r';'-*g';'-*b';'-*c';'-*m';'-*y';'-*k';'-*w'];
+colors_err = ['rx';'gx';'bx';'cx';'mx';'yx';'kx';'wx'];
+
+cv = dlmread('./c_v_split.txt', ',');
+
+hp_1 = cv(:,1);
+hp_2 = cv(:,2);
+
+x = cv(:,4);
+y = cv(:,3);
+sd = cv(:,5);
+
+[me, ind] = min(y);
+if me < min_error
+    min_error = me;
+    min_params = [hp_1(ind), hp_2(ind), x(ind)];
+end
+
+prev_i = 1;
+c = 1;
+for i = 2:length(hp_1)
+    if hp_1(i) ~= hp_1(i-1) || hp_2(i) ~= hp_2(i-1)
+        mat = [x(prev_i:i-1), y(prev_i:i-1), sd(prev_i:i-1)];
+        [values, order] = sort(mat(:,1));
+        m = mat(order,:);
+        
+        plot(m(:,1),m(:,2),colors(c,:));
+        hold on;
+        %errorbar(m(:,1),m(:,2),m(:,3),colors_err(c,:));
+        
+        c = mod(c+1,size(colors,1));
+        if c == 0
+            c = c+1;
+        end
+        prev_i = i;
+        hold on;
+    end
+end
+if prev_i < length(hp_1)
+    mat = [x(prev_i:length(hp_1)), y(prev_i:length(hp_1)), sd(prev_i:length(hp_1))];
+    [values, order] = sort(mat(:,1));
+    m = mat(order,:);
+    plot(m(:,1),m(:,2),colors(c,:));
+    hold on;
+    %errorbar(m(:,1),m(:,2),m(:,3),colors_err(c,:));
+    
+    hold on;
+end
+
+% set of \tau parameters 
+
+% close curent opening figures
+
+
+
+% plotting test errors versus different tau parameters
+%plot(x, y, '-*r');
+ylabel('Cross Validation Error');
+title('Cross Validation Error for Each Split Size');
+xlabel('Number of features considered at each split');
+%axis([min(x)-1 max(x)+1 min(y)-0.01 max(y)+0.01]);
+grid on;
+
+saveas(gcf, 'cv_split.fig');
+
+hold off;
+
+cv = dlmread('./c_v_depth.txt', ',');
+
+hp_1 = cv(:,1);
+hp_2 = cv(:,2);
+
+x = cv(:,4);
+y = cv(:,3);
+sd = cv(:,5);
+
+prev_i = 1;
+c = 1;
+
+[me, ind] = min(y);
+if me < min_error
+    min_error = me;
+    min_params = [hp_1(ind), x(ind), hp_2(ind)];
+end
+
+for i = 2:length(hp_1)
+    if hp_1(i) ~= hp_1(i-1) || hp_2(i) ~= hp_2(i-1)
+        mat = [x(prev_i:i-1), y(prev_i:i-1), sd(prev_i:i-1)];
+        [values, order] = sort(mat(:,1));
+        m = mat(order,:);
+        
+        plot(m(:,1),m(:,2),colors(c,:));
+        hold on;
+        %errorbar(m(:,1),m(:,2),m(:,3),colors_err(c,:));
+        
+        c = mod(c+1,size(colors,1));
+        if c == 0
+            c = c+1;
+        end
+        prev_i = i;
+        hold on;
+    end
+end
+
+if prev_i < length(hp_1)
+    mat = [x(prev_i:length(hp_1)), y(prev_i:length(hp_1)), sd(prev_i:length(hp_1))];
+    [values, order] = sort(mat(:,1));
+    m = mat(order,:);
+    plot(m(:,1),m(:,2),colors(c,:));
+    hold on;
+    %errorbar(m(:,1),m(:,2),m(:,3),colors_err(c,:));
+    
+    hold on;
+end
+% set of \tau parameters 
+
+% close curent opening figures
+%close all;
+
+% plotting test errors versus different tau parameters
+%plot(x, y, '-*r');
+ylabel('Cross Validation Error');
+title('Cross Validation Error for Each Depth');
+xlabel('Depth of Tree');
+%axis([min(x)-1 max(x)+1 min(y)-0.01 max(y)+0.01]);
+grid on;
+
+saveas(gcf, 'cv_depth.fig');
+
+hold off;
+cv = dlmread('./c_v_K.txt', ',');
+
+hp_1 = cv(:,1);
+hp_2 = cv(:,2);
+
+x = cv(:,4);
+y = cv(:,3);
+sd = cv(:,5);
+
+[me, ind] = min(y);
+if me < min_error
+    min_error = me;
+    min_params = [x(ind), hp_1(ind), hp_2(ind)];
+end
+
+prev_i = 1;
+c = 1;
+
+for i = 2:length(hp_1)
+    if hp_1(i) ~= hp_1(i-1) || hp_2(i) ~= hp_2(i-1)
+        mat = [x(prev_i:i-1), y(prev_i:i-1), sd(prev_i:i-1)];
+        [values, order] = sort(mat(:,1));
+        m = mat(order,:);
+        
+        plot(m(:,1),m(:,2),colors(c,:));
+        hold on;
+        %errorbar(m(:,1),m(:,2),m(:,3),colors_err(c,:));
+        
+        c = mod(c+1,size(colors,1));
+        if c == 0
+            c = c+1;
+        end
+        prev_i = i;
+        hold on;
+    end
+end
+if prev_i < length(hp_1)
+    mat = [x(prev_i:length(hp_1)), y(prev_i:length(hp_1)), sd(prev_i:length(hp_1))];
+    [values, order] = sort(mat(:,1));
+    m = mat(order,:);
+    plot(m(:,1),m(:,2),colors(c,:));
+    hold on;
+    %errorbar(m(:,1),m(:,2),m(:,3),colors_err(c,:));
+    
+    hold on;
+end
+% plotting test errors versus different tau parameters
+%plot(x, y, '-*r');
+ylabel('Cross Validation Error');
+title('Cross Validation Error per Number of Topics');
+xlabel('Number of Topics');
+%axis([min(x)-1 max(x)+1 min(y)-0.01 max(y)+0.01]);
+grid on;
+
+saveas(gcf, 'cv_K.fig');
+close all;
+
+[values, order] = sort(cv(:,3));
+m = cv(order,:);
+disp([m(1:4,4), m(1:4,1), m(1:4,2), m(1:4,3)]);
+
+
+end
